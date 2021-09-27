7131fe2d6dfe3aa8e2f797e8f62482cb418e80d3
tgoodric <tdgoodrich1@gmail.com>
2021-09-27 16:19:14 -0400

    further dao implementation

--- src/main/java/com/revature/daos/ReimbursementDao.java
+++ src/main/java/com/revature/daos/ReimbursementDao.java
@@ -3,30 +3,68 @@
  */
 package com.revature.daos;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 import com.revature.models.Reimbursement;
+import com.revature.utils.ConnectionUtils;
 
 /**
- * @author tdgoo
+ * @author T. D. Goodrich
  *
  */
 public class ReimbursementDao implements IReimbursementDao {
 
 	/**
-	 * 
+	 * Default Ctor, no implementation
 	 */
 	public ReimbursementDao() {
-		// TODO Auto-generated constructor stub
+		super();
+		//no implementation
 	}
 
 	@Override
 	public ArrayList<Reimbursement> getReimbursements() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "select * from reimbursements";
+		ArrayList<Reimbursement> reimbursements = null;
+		try(Connection conn = ConnectionUtils.getConnection()){
+			Statement s = conn.createStatement();
+			ResultSet rs = s.executeQuery(sql);
+			reimbursements = generateResults(rs);
+			return reimbursements;
+		}
+		catch (SQLException e) {
+			StringBuilder sb = new StringBuilder("Error occurred while retrieving all reimbursements: \nSQL State: ");
+			sb.append(e.getSQLState());
+			sb.append("\nVendor Error Code: ");
+			sb.append(e.getErrorCode());
+			throw new SQLException(sb.toString());
+		}
 	}
 
+	private ArrayList<Reimbursement> generateResults(ResultSet rs) throws SQLException {
+		ArrayList<Reimbursement> reimbursements;
+		reimbursements = new ArrayList<Reimbursement>();
+		while(rs.next()) {
+			Reimbursement r = new Reimbursement(
+					rs.getInt("reimbursement_id"),
+					rs.getDouble("amount"),
+					rs.getDate("submitted"),
+					rs.getDate("resolved"),
+					rs.getString("description"),
+					null, //TODO: figure out how to translate from BYTEA to Image
+					rs.getInt("submitter_id_fk"),
+					rs.getInt("resolver_id_fk"),
+					rs.getInt("reimbursement_type_fk"),
+					rs.getInt("reimbursement_status_fk"));
+			reimbursements.add(r);
+		}
+		return reimbursements;
+	}
+	
 	@Override
 	public ArrayList<Reimbursement> getReimbursements(int id) throws SQLException {
 		// TODO Auto-generated method stub
